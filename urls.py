bdb4a33997881312f017b3872bcf36a49bebf0fa
hisham waleed karam <hisham.karam@cartologic.com>
2018-11-25 15:43:23 +0200

    fix django1.11 compatibility

--- urls.py
+++ urls.py
@@ -1,7 +1,5 @@
-from . import views
-from django.conf.urls import patterns, url
-from . import APP_NAME
-urlpatterns = urls = patterns('',
-                              url(r'^$', views.index, name="%s.index" %
-                                  (APP_NAME)),
-                              )
+from django.conf.urls import url
+
+from . import APP_NAME, views
+
+urlpatterns = urls = [url(r'^$', views.index, name="%s.index" % (APP_NAME)), ]
